89bc0df2750173e87e51d4c87095c89f566c9c8a
Gustavo Rodrigues <102631537+GuuhRodrigues@users.noreply.github.com>
2022-06-04 18:23:11 -0300

    Add files via upload
    Faça  um  algoritmo  que  alimente  um  array  com  os  valores  1  a  10  números 
    automaticamente.  
    Imprima o array resultante.

---
+++ Ex57.java
@@ -0,0 +1,13 @@
+public class Ex57{
+	public static void main(String[] args){
+
+		int[] vetnum = new int[10];
+
+		for(int i=0;i<vetnum.length;++i){
+
+			vetnum[i]=i+1;
+
+		System.out.print(vetnum[i]+" ");
+		}
+	}
+}
